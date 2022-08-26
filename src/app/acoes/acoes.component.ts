4a3ca14d6cc9a549b2e084e8136a29c50650b8ff
Yuri Bessa <yuri14.levita@gmail.com>
2022-08-26 18:07:16 -0300

    Comitando os arquivos iniciais

---
+++ src/app/acoes/acoes.component.ts
@@ -0,0 +1,20 @@
+import { AcoesService } from './acoes.service';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { tap, switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css'],
+})
+export class AcoesComponent {
+  acoesInput = new FormControl();
+  acoes$ = this.acoesInput.valueChanges.pipe(
+    tap(console.log),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+    tap(console.log)
+  );
+
+  constructor(private acoesService: AcoesService) {}
+}
